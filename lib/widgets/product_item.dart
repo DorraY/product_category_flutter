08a211ea089ed7a1f859d2a790406e207a920311
DorraY <youssefdorra@gmail.com>
2021-11-28 19:01:19 +0100

    update layout

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:product_category/models/product.dart';
@@ -53,9 +54,11 @@ class ProductItem extends StatelessWidget {
                           mainAxisAlignment: MainAxisAlignment.end,
                           children: [
                             IconButton(
+                              color: Colors.white,
                                 onPressed: () => deleteProduct(product.id),
                                 icon: const Icon(Icons.delete)),
                             IconButton(
+                                color: Colors.white,
                                 onPressed: () =>
                                     editProductModalSheet(context, product.id),
                                 icon: const Icon(Icons.auto_fix_high))
@@ -96,112 +99,37 @@ class ProductItem extends StatelessWidget {
                 )
               ],
             ) : Column(children: <Widget>[
-
-
+              Row(
+                children: <Widget>[
+                  const Icon(Icons.schedule),
+                  const SizedBox(width: 6),
+                  Text(
+                      "${(DateFormat.yMd().format(product.expiryDate))}",textAlign: TextAlign.left,
+                  overflow: TextOverflow.fade,
+                  ),
+                ],
+              ),
               Row(
                 children: <Widget>[
                   const Icon(Icons.face),
                   const SizedBox(width: 6),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width*0.1,
-                    child: FittedBox(
-                      fit: BoxFit.contain,
-                      child: Text(
-                          "${product.category!.title}",textAlign: TextAlign.left),
-                    ),
-                  ),
+                  Text(
+                      "${product.category!.title}",textAlign: TextAlign.left),
                 ],
               ),
               Row(
                 children: <Widget>[
                   const Icon(Icons.monetization_on),
                   const SizedBox(width: 6),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width*0.05,
-                    child: FittedBox(
-                      fit: BoxFit.contain,
-                      child: Text(
-                          "${product.price}",textAlign: TextAlign.left),
-                    ),
-                  ),
+                  Text(
+                      "${product.price}",textAlign: TextAlign.left),
                 ],
               ),
-              Row(
-                children: <Widget>[
-                  const Icon(Icons.schedule),
-                  const SizedBox(width: 6),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width*0.5,
-                    child: FittedBox(
-                      fit: BoxFit.contain,
-                      child: Text(
-                          "Expires in ${(DateFormat.yMMMd().format(product.expiryDate))}",textAlign: TextAlign.left),
-                    ),
-                  ),
-                ],
-              ),
-
             ] ,
             mainAxisAlignment: MainAxisAlignment.start,),
           ),
         ],
       ),
     );
-    /*return Card(
-      elevation: 5,
-      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-      child: ListTile(
-        leading: Container(
-          height: 60,
-          width: 60,
-          decoration: BoxDecoration(
-              color: Theme.of(context).primaryColor, shape: BoxShape.circle),
-          child: Padding(
-            padding: const EdgeInsets.all(10),
-            child: FittedBox(
-                child: Text(
-                  '\$${product.price}',
-                  style: const TextStyle(color: Colors.white),
-                )),
-          ),
-        ),
-        title: product.category!=null ? Row(children: [
-          Text('${product.name} - '),
-          Text('${product.category!.title}',style: TextStyle(fontWeight: FontWeight.bold))
-        ],) :  Text('${product.name}'),
-
-        subtitle: Text('Expires in ${DateFormat.yMMMd().format(product.expiryDate)}'),
-        trailing: SizedBox(
-          width: MediaQuery.of(context).size.width * 0.3,
-          child: MediaQuery.of(context).size.width > 550
-              ? Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              TextButton.icon(
-                  label: const Text('Delete'),
-                  onPressed: () => deleteProduct(product.id),
-                  icon: const Icon(Icons.delete)),
-              TextButton.icon(
-                  label: const Text('Edit'),
-                  onPressed: () =>
-                      editProductModalSheet(context, product.id),
-                  icon: const Icon(Icons.auto_fix_high))
-            ],
-          )
-              : Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              IconButton(
-                  onPressed: () => deleteProduct(product.id),
-                  icon: const Icon(Icons.delete)),
-              IconButton(
-                  onPressed: () =>
-                      editProductModalSheet(context, product.id),
-                  icon: const Icon(Icons.auto_fix_high))
-            ],
-          ),
-        ),
-      ),
-    );*/
   }
 }
